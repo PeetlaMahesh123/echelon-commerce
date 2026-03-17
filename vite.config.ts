13b5133e2a7c94663a9bae40f305a84f2dfd8863
peetlamahesh <peetlamahesh81@gmail.com>
2026-03-17 19:17:16 +0530

    Fix: Use correct base path for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/echelon-commerce/",
+  base: mode === 'production' ? '/echelon-commerce/' : '/',
   server: {
     host: "::",
     port: 8080,
